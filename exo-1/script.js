f1be3442e748d3643c415139eb8bff891dac4517
Mohammed-Fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-01-06 13:15:18 +0100

    exo 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,16 @@
-// you can write js here
-console.log('hello from file');
+// Demande la température en Kelvins à l'utilisateur
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+// Pour convertir de Kelvins en Celsius, on soustrait 273
+let celsius = kelvins - 273;
+
+// La formule est : Fahrenheit = Celsius * (9/5) + 32
+let fahrenheits = celsius * (9 / 5) + 32;
+
+// Utilisation de Math.floor() pour supprimer les décimales
+fahrenheits = Math.floor(fahrenheits);
+
+// Affichage des résultats
+console.log("La température est de " + kelvins + " Kelvins");
+console.log("La température est de " + celsius + " degrés Celsius");
+console.log("La température est de " + fahrenheits + " degrés Fahrenheit");
