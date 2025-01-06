9fcdcbf92580ee8ade4db496e989a261e2bb7b4f
Mohammed-Fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-01-06 16:49:33 +0100

    exo 8

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,61 @@
 console.log("exo-7");
 
-console.log(jsonDatas);
+console.log("1. Affichage des articles:");
+jsonDatas.forEach((article) => {
+  console.log(article);
+});
+
+const typeTranslations = {
+  car: "Voitures",
+  house: "Maisons",
+  game: "Jeux",
+  videoGame: "Jeux Vidéos",
+  show: "Spectacles",
+};
+
+console.log("2. Objet de traduction:", typeTranslations);
+
+const articlesWithTranslation = jsonDatas.map((article) => {
+  return {
+    ...article,
+    translatedType: typeTranslations[article.type] || article.type,
+  };
+});
+
+console.log("3. Articles avec traduction:");
+articlesWithTranslation.forEach((article) => {
+  console.log(
+    `Article: ${article.name}, Type: ${article.type}, Type traduit: ${article.translatedType}`
+  );
+});
+
+function transformData(originalData) {
+  const groupedByType = originalData.reduce((acc, item) => {
+    if (!acc[item.type]) {
+      acc[item.type] = {
+        type: item.type,
+        items: [],
+      };
+    }
+
+    const itemWithContact = {
+      ...item,
+      contact: {
+        lastName: "Luka",
+        firstName: "Modric",
+        address: "Luka.Modric@gmail.cpm",
+      },
+    };
+
+    delete itemWithContact.type;
+
+    acc[item.type].items.push(itemWithContact);
+    return acc;
+  }, {});
+
+  return Object.values(groupedByType);
+}
+
+const newJsonDatas = transformData(jsonDatas);
+
+console.log("Structure transformée :", newJsonDatas);
