e2f5d7feedb042ecbc46d4d81cd1c3468b13e52c
Mohammed-Fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-01-06 14:05:17 +0100

    exo 5

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,64 @@
-// you can write js here
+let secretMessage = [
+  "Learning",
+  "isn't",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it's",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
 
-console.log('exo-4');
+// Afficher le tableau initial
+console.log("Message initial :");
+console.log(secretMessage.join(" "));
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+// 1. Enlever le dernier élément
+secretMessage.pop();
+console.log("\nAprès suppression du dernier élément :");
+console.log(secretMessage.join(" "));
+
+// 2. Ajouter 'to' et 'program' à la fin
+secretMessage.push("to", "program");
+console.log("\nAprès ajout de 'to' et 'program' :");
+console.log(secretMessage.join(" "));
+
+// 3. Remplacer 'easily' par 'right'
+const easilyIndex = secretMessage.indexOf("easily");
+secretMessage[easilyIndex] = "right";
+console.log("\nAprès remplacement de 'easily' par 'right' :");
+console.log(secretMessage.join(" "));
+
+// 4. Supprimer le premier élément
+secretMessage.shift();
+console.log("\nAprès suppression du premier élément :");
+console.log(secretMessage.join(" "));
+
+// 5. Ajouter 'Programming' au début
+secretMessage.unshift("Programming");
+console.log("\nAprès ajout de 'Programming' au début :");
+console.log(secretMessage.join(" "));
+
+// 6. Remplacer 5 mots par 'know'
+const getIndex = secretMessage.indexOf("get");
+secretMessage.splice(getIndex, 5, "know");
+console.log("\nAprès remplacement des 5 mots par 'know' :");
+console.log(secretMessage.join(" "));
+
+// 7. Afficher le message final
+console.log("\nMessage secret final :");
+console.log(secretMessage.join(" "));
