58786e2d80bc1fc55b5006186b47a79ecf40e5ba
Mohammed-Fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-01-06 14:23:22 +0100

    exo 6

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,35 @@
-// you can write js here
+const input = "Turpentine and turtles";
 
-console.log('exo-5');
+const vowels = ["a", "e", "i", "o", "u"];
+
+const resultArray = [];
+
+function isVowel(letter) {
+  let found = false;
+  for (let j = 0; j < vowels.length; j++) {
+    if (letter.toLowerCase() === vowels[j]) {
+      found = true;
+      break;
+    }
+  }
+  return found;
+}
+
+for (let i = 0; i < input.length; i++) {
+  console.log("Lettre courante:", input[i]);
+
+  /* if (vowels.indexOf(input[i].toLowerCase()) !== -1) {
+        resultArray.push(input[i]);
+    }*/
+
+  if (isVowel(input[i])) {
+    resultArray.push(input[i]);
+  }
+}
+
+console.log("Voyelles trouvées:", resultArray);
+
+const whaleTranslation = resultArray.join("").toUpperCase();
+
+console.log("\nPhrase originale:", input);
+console.log("Traduction en langue des baleines:", whaleTranslation);
